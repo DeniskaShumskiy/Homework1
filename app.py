1d68243b83a779874cc3442f96904a93c2341ed0
Denis <shumik16@gmail.com>
2024-02-05 13:30:34 +0300

    Good

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 print("I’m planning to learn git conflicts resolution")
+<<<<<<< HEAD
 name = input("Please enter your name ")
 surname = input("Please enter your surname ")
 print("Hi, ", name, surname)
+=======
+nickname = input("Please enter your nickname ")
+print("Hi", nickname)
+>>>>>>> Denis_Shumskiy
